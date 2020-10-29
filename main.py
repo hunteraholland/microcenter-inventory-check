d847fd8ceff56d7f8abd8a287b22739db8596a4a
Hunter Holland <huntholland@gmail.com>
2020-10-29 18:44:15 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+import re
+from bs4 import BeautifulSoup as bs
+
+r = requests.get("https://www.microcenter.com/product/601213/corsair-sf750-750-watt-80-plus-platinum-sfx-fully-modular-power-supply?storeid=141")
+
+src = r.content
+
+soup = bs(src, features="html.parser")
+
+print(soup.find("span", {"class" : "inventoryCnt"}))
