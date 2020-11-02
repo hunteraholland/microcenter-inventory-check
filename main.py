6c12f8cb45ec039dcb0eae17f27efead1f690c35
Hunter Holland <huntholland@gmail.com>
2020-11-01 20:02:11 -0500

    Add class structure

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import requests
 import re
 from bs4 import BeautifulSoup as bs
+import smtplib, ssl
 
-r = requests.get("https://www.microcenter.com/product/601213/corsair-sf750-750-watt-80-plus-platinum-sfx-fully-modular-power-supply?storeid=141")
+class CheckInventory:
 
-src = r.content
+    def get_status(self):
+        r = requests.get("https://www.microcenter.com/product/601213/corsair-sf750-750-watt-80-plus-platinum-sfx-fully-modular-power-supply?storeid=141")
+        src = r.content
+        soup = bs(src, features="html.parser")
+        inventory = soup.find("span", {"class" : "inventoryCnt"})
 
-soup = bs(src, features="html.parser")
+        # if inventory.text != "Sold Out":
+        #     print("true")
+        # else:
+        #     print('false')
 
-inventory = soup.find("span", {"class" : "inventoryCnt"})
+        return inventory.text
 
-if inventory.text == "Sold Out":
-    print("true")
-else:
-    print("false")
+    def send_email(self):
+        print('an email was sent')
+
+def main():
+    test = CheckInventory()
+
+    if test.get_status() == 'Sold Out':
+        test.send_email()
+
+if __name__ == '__main__': main()
+        
+        # def send_email():
+        # # port = 465  # For SSL
+        # # password = input("Type your password and press enter: ")
+        # # context = ssl.create_default_context()
+
+        # # with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        # #     server.login("development@hunterholland.com", password)
+        #     print('Hey')
