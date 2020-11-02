4e780c48227c5a903aff291c4fe919a8210d4fde
Hunter Holland <huntholland@gmail.com>
2020-11-01 20:15:33 -0500

    Add message to send_message function

--- main.py
+++ main.py
@@ -12,17 +12,26 @@ class CheckInventory:
         inventory = soup.find("span", {"class" : "inventoryCnt"})
         return inventory.text
 
-    def get_connection(self):
+    def send_email(self):
         port = 465  # For SSL
+        smtp_server = "smtp.gmail.com"
+        sender_email = "my@gmail.com"  # Enter your address
+        receiver_email = "your@gmail.com"  # Enter receiver address
         password = input("Type your password and press enter: ")
+        message = """\
+        Subject: Hi there
+
+        This message is sent from Python."""
+
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-            server.login("development@hunterholland.com", password)
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
 
 def main():
     test = CheckInventory()
     if test.get_status() != 'Sold Out':
-        test.get_connection()
+        test.send_email()
 
 if __name__ == '__main__': main()
         
