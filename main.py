9640efedd745d45ae4a91ce4d1b1d8748f3664f3
Hunter Holland <huntholland@gmail.com>
2020-11-02 18:10:47 -0500

    Fix email message

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import requests
 import re
 from bs4 import BeautifulSoup as bs
 import smtplib, ssl
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 
 class CheckInventory:
 
@@ -17,42 +15,18 @@ class CheckInventory:
     def send_email(self):
         port = 465  # For SSL
         smtp_server = "smtp.gmail.com"
-        sender_email = "hunterhollandev@gmail.com"
-        from_email = "development@hunterholland.com"
-        receiver_email = "huntholland@gmail.com"
+        sender_email = "hunterhollandev@gmail.com"  # Enter your address
+        receiver_email = "huntholland@gmail.com"  # Enter receiver address
         password = input("Type your password and press enter: ")
+        message = """\
+        Subject: Hi there
 
-        message = MIMEMultipart("alternative")
-        message["Subject"] = "Get Yo Powasupply"
-        message["From"] = from_email
-        message["To"] = receiver_email
-        text = """\
         Greetings Hunter,
+
         You're a beast and your program worked. Welcome to the sff pc game.
+
         Sincerely,
-        Hunter Holland LLC Dev Team
-        """
-        html = """\
-        <html>
-        <body>
-            <p>Greetings Hunter,<br>
-            You're a beast and your program worked. Welcome to the sff pc game.<br>
-            Sincerely, <br>
-            Hunter Holland LLC Dev Team
-            </p>
-        </body>
-        </html>
-        """
-
-        # Turn these into plain/html MIMEText objects
-        part1 = MIMEText(text, "plain")
-        part2 = MIMEText(html, "html")
-
-        # Add HTML/plain-text parts to MIMEMultipart message
-        # The email client will try to render the last part first
-        message.attach(part1)
-        message.attach(part2)
-
+        Hunter Holland LLC Dev Team"""
 
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
@@ -61,8 +35,8 @@ class CheckInventory:
 
 def main():
     test = CheckInventory()
-    if test.get_status() == 'Sold Out':
-        test.send_email()
+    # if test.get_status() == 'Sold Out':
+    test.send_email()
 
 if __name__ == '__main__': main()
         
